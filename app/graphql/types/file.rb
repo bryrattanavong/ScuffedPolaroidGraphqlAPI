cdb19124818700a9d8126a677f31d56225b9fae7
bryrattanavong <arandomexample@hotmail.com>
2020-03-12 01:22:25 -0400

    correcting file type

--- app/graphql/types/file.rb
+++ app/graphql/types/file.rb
@@ -1,16 +1,16 @@
-module Types
-    Scalars::FileType = GraphQL::ScalarType.define do
-        name 'File'
-        description 'action_dispatch_uploaded_file'
-        coerce_input lambda { |file, _ctx|
-          return nil if file.nil?
-      
-          ActionDispatch::Http::UploadedFile.new(
-            filename: file.original_filename,
-            type: file.content_type,
-            head: file.headers,
-            tempfile: file.tempfile
-          )
-        }
+module Scalars
+    class File < Scalars::BaseScalar
+      description 'action_dispatch_uploaded_file'
+  
+      def self.coerce_input(_input_value, _context)
+        return nil if file.nil?
+  
+        ActionDispatch::Http::UploadedFile.new(
+          filename: file.original_filename,
+          type: file.content_type,
+          head: file.headers,
+          tempfile: file.tempfile
+        )
       end
-end
+    end
+  end
