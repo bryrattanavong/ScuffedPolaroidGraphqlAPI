3efc26de47687860d88c0d371a29ad517ac9616c
bryrattanavong <arandomexample@hotmail.com>
2020-03-15 22:03:11 -0400

    added image model + changes

---
+++ app/models/image.rb
@@ -0,0 +1,12 @@
+include Rails.application.routes.url_helpers
+class Image < ApplicationRecord
+
+  belongs_to :user
+  has_one_attached :image
+  validates :image, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg']
+
+  def image_url
+    Rails.application.routes.url_helpers.rails.blob.url(image)
+  end
+
+end
