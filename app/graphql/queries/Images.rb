5f1b7433c9b1275313f03dad23866a352079855d
bryrattanavong <arandomexample@hotmail.com>
2020-03-20 23:27:29 -0400

    clean up

--- app/graphql/queries/Images.rb
+++ app/graphql/queries/Images.rb
@@ -1,15 +1,10 @@
 module Queries
   class Images < BaseQuery
+    
     type Types::ImageType.connection_type, null: false
 
-    argument :title, String, required: false
-
-    def resolve(title: nil)
-      if title.present?
-        Image.where('title LIKE ?', "%#{title}%")
-      else
-        Image.all
-      end
+    def resolve
+     Image.all
     end
   end
 end
