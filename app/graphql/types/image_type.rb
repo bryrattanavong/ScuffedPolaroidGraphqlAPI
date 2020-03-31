0695a8a464c5c33656c0094da71f72911601bb49
bryrattanavong <arandomexample@hotmail.com>
2020-03-31 02:29:33 -0400

    graphql person type

--- app/graphql/types/image_type.rb
+++ app/graphql/types/image_type.rb
@@ -4,6 +4,7 @@ module Types
         field :image_url, String, null: false
         field :description, String, null: true
         field :hash_tags,[HashTagType], null:true
+        field :people,[PersonType], null:true
         field :posted_by, UserType, null: true, method: :user
     end
 end
