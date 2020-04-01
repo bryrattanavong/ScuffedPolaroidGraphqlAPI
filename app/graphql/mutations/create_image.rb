a5d63c8df0d77e1f0b13b9b35102567cefb00d89
bryrattanavong <arandomexample@hotmail.com>
2020-03-31 21:33:24 -0400

    update create-image

--- app/graphql/mutations/create_image.rb
+++ app/graphql/mutations/create_image.rb
@@ -1,37 +1,34 @@
 module Mutations
-    class CreateImage < BaseMutation
-      argument :image, Types::FileType, required: true
-      argument :description, String, required: false
-    
-      type Types::ImageType
+  class CreateImage < BaseMutation
+    argument :image, Types::FileType, required: true
+    argument :description, String, required: false
   
-      def resolve(image:, description: nil)
-        user = context[:current_user]
-        image = Image.create(
-          image: image,
-          description: description,
-          user: user
-        )
-        raise GraphQL::ExecutionError, image.errors.full_messages.join(", ") unless image.errors.empty?
-        if description
-          tags = description.to_s.scan(/#\w+/).map{|tag| tag.gsub("#", "")}
-          if tags
-            tags.each do |tag|
-              hash_tag = HashTag.find_by(name: tag)
-              if hash_tag.nil?
-                hash_tag = HashTag.create(
-                  name: tag
-                )
-              end
-            image_hashtag = ImageHashTag.create(
-              image_id: image.id,
-              hash_tag_id: hash_tag.id
+    type Types::ImageType
+
+    def resolve(image:, description: nil)
+      user = context[:current_user]
+      image = Image.create(
+        image: image,
+        description: description,
+        user: user
+      )
+      raise GraphQL::ExecutionError, image.errors.full_messages.join(", ") unless image.errors.empty?
+      if image.present?
+        description.to_s.scan(/#\w+/).map{|tag| tag.gsub("#", "")}.each do |tag|
+          hash_tag = HashTag.find_by(name: tag)
+          if hash_tag.nil?
+            hash_tag = HashTag.create(
+              name: tag
             )
-            raise GraphQL::ExecutionError, image_hashtag.errors.full_messages.join(", ") unless image_hashtag.errors.empty?   
-            end
           end
+          image_hashtag = ImageHashTag.create(
+            image_id: image.id,
+            hash_tag_id: hash_tag.id
+          )
+          raise GraphQL::ExecutionError, image_hashtag.errors.full_messages.join(", ") unless image_hashtag.errors.empty?   
         end
-        image 
       end
+    image
     end
-  end 
+  end
+end 
