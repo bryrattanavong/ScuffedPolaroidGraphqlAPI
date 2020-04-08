0d5f6983ce89c1684e1746ca397ab12b1cb788fd
bryrattanavong <arandomexample@hotmail.com>
2020-04-08 17:00:09 -0400

    setting up auth

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -1,4 +1,8 @@
 module Mutations
   class Mutations::BaseMutation < GraphQL::Schema::Mutation
+    def authorized_user
+      return if context[:current_user]
+      raise ExceptionHandler::InvalidToken, 'Token verification failed'
+    end
   end
 end
